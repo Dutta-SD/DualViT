168c532900a32dd591ee2d8b24201afdbe56051e
Sandip Dutta <duttasandip11100@gmail.com>
2023-10-02 00:10:57 +0530

    pl

--- benchmark.py
+++ benchmark.py
@@ -5,19 +5,19 @@ from transformers import ViTConfig
 
 from vish.model.tp.single_split import SplitHierarchicalTPViT
 
-DEVICE = torch.device("cpu")
+DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 # from vish.utils import DEVICE
 
-model = SplitHierarchicalTPViT(ViTConfig())
+model = SplitHierarchicalTPViT(ViTConfig()).to(DEVICE)
 
 
 def time_model(fxn, exp_name, cuda):
     times = []
-    for _ in range(5):
+    for _ in range(10):
         if cuda:
             torch.cuda.synchronize()
-        rand_tensor = torch.randn(4, 3, 224, 224, device=DEVICE)
+        rand_tensor = torch.randn(8, 3, 224, 224, device=DEVICE)
 
         start_epoch = time.time()
 
@@ -28,8 +28,8 @@ def time_model(fxn, exp_name, cuda):
         end_epoch = time.time()
         elapsed = end_epoch - start_epoch
         times.append(elapsed)
-    print(f"{exp_name} time needed: ", sum(times) / 5)
+    print(f"{exp_name} time needed: ", sum(times) / 10)
 
 
-time_model(model.forward, "Normal Forward", False)
-time_model(model.forward2, "Forward 2", False)
+time_model(model.forward, "Normal Forward", True)
+time_model(model.forward2, "Forward 2", True)
