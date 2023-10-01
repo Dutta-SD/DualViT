3c1b3b887c30d2048edf60841b1cce4997fae3d5
Sandip Dutta <duttasandip11100@gmail.com>
2023-10-02 01:43:13 +0530

    decomposed cleared

--- train_decomposed.py
+++ train_decomposed.py
@@ -1,35 +1,46 @@
 import sys
 from datetime import datetime
 
-from vish.model.decomposed.decomposed import VitClassificationDecomposed
+import torch
+
+from vish.constants import (
+    DATE_TIME_FORMAT,
+    EPOCHS,
+    IS_TEST_RUN,
+    LEARNING_RATE,
+    LOAD_CKPT,
+    MIN_LR,
+    MOMENTUM,
+    WEIGHT_DECAY,
+    WEIGHT_FOLDER_PATH,
+)
 from vish.model.common.tree import LabelHierarchyTree
+from vish.model.decomposed.decomposed import VitClassificationDecomposed
 from vish.trainer.decomposed import VitDecomposedTrainer
-from vish.utils import *
+from vish.utils import DEVICE, accuracy, test_dl, to_device, train_dl
 
-# Set this flag to True if you want to just test the thing.
-# For running complete experiments, set it to False
-TEST = False
+# TODO: Overwrite for every train file
+DESC = "vit-decomposed-cifar10"
+CKPT_DESC = ""
 
-DATE_TIME_FORMAT = "%Y_%m_%d_%H_%M_%S"
 CURR_TIME = datetime.now().strftime(DATE_TIME_FORMAT)
 
 LOG_FILE_NAME = f"logs/EXP-{CURR_TIME}_{DESC}.txt"
 
-
-if TEST:
-    LOG_FILE_NAME = f"logs/TEST-{DESC}.txt"
-    EPOCHS = 20
-
+if IS_TEST_RUN:
+    DESC = f"TEST-{DESC}"
+    LOG_FILE_NAME = f"logs/{DESC}.txt"
+    EPOCHS = 30
 
 log_file = open(LOG_FILE_NAME, "a")
+print("Logging @:", LOG_FILE_NAME)
 sys.stdout = log_file
 
+ckpt = None
 
-LOAD = False
-
-if LOAD:
-    # DESC = "vit-b-16-dual-model-pretrained-both_1694963453.pt"
-    ckpt = torch.load(f"./checkpoints/{DESC}")
+if LOAD_CKPT:
+    DESC = CKPT_DESC
+    ckpt = torch.load(f"./checkpoints/{DESC}.pt")
     print("Checkpoint Loaded...")
 
 
@@ -37,9 +48,9 @@ if LOAD:
 print("*" * 120)
 print("Started at: ", CURR_TIME)
 
+# Model Definition
 # Label tree
 lt = LabelHierarchyTree("vish/data/cifar10.xml")
-
 
 # MODEL - CIFAR 10
 model = VitClassificationDecomposed(
@@ -49,11 +60,14 @@ model = VitClassificationDecomposed(
     patch_dim=16,
     emb_dim=768,
     label_tree=lt,
-    num_blocks_per_group=3,
+    max_depth_before_clf=2,
+    num_blocks_per_group=2,
 )
 
-# model = ckpt["model"]
-print(model)
+if LOAD_CKPT:
+    model = ckpt["model"]
+    print("Model Loaded from checkpoint")
+# print(model)
 
 model = to_device(model, DEVICE)
 
@@ -61,15 +75,19 @@ optimizer = torch.optim.SGD(
     model.parameters(),
     lr=LEARNING_RATE,
     weight_decay=WEIGHT_DECAY,
-    momentum=0.9,
+    momentum=MOMENTUM,
 )
-# optimizer.load_state_dict(ckpt["opt"][0])
+
+if LOAD_CKPT:
+    optimizer.load_state_dict(ckpt["opt"][0])
+    print("Optimizer loaded from checkpoint")
 
 scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
     optimizer,
     mode="max",
     verbose=True,
     min_lr=MIN_LR,
+    patience=5,
 )
 
 # Training
@@ -81,17 +99,23 @@ trainer_params = {
     "optimizer_list": [optimizer],
     "scheduler_list": [scheduler],
     "metrics_list": [("Acc@1", accuracy)],
-    "best_score_key": "Acc@1",  # Set it as per need
+    "best_score_key": "Acc@1_fine",  # Set it as per need - for dual specify broad or fine
     "model_checkpoint_dir": WEIGHT_FOLDER_PATH,
     "description": DESC,
-    # "uniq_desc": False,  # Uncomment if loading checkpoint
-    # "best_train_score": ckpt["best_train_score"],
-    # "best_test_score": ckpt["best_test_score"],
 }
+
+
+if LOAD_CKPT:
+    load_kwargs = {
+        "uniq_desc": False,  # Uncomment if loading checkpoint
+        "best_train_score": ckpt["best_train_score"],
+        "best_test_score": ckpt["best_test_score"],
+    }
+    trainer_params = {**trainer_params, **load_kwargs}
+
 trainer = VitDecomposedTrainer(**trainer_params)
 run_kwargs = {}
 
 trainer.run(**run_kwargs)
-
 
 print(f"Final Model saved @ {trainer.ckpt_full_path}")
--- vish/trainer/decomposed.py
+++ vish/trainer/decomposed.py
@@ -6,24 +6,8 @@ from torch import nn
 from vish.model.decomposed.decomposed import VitClassificationDecomposed
 from vish.model.decomposed.entity import TransformerData
 from vish.trainer.base import BaseTrainer
-from vish.utils import DEVICE
 
 os.environ["TOKENIZERS_PARALLELISM"] = "true"
-
-
-def rearrange_outputs(class_wise_logits: dict[str, TransformerData], num_outputs):
-    # y_pred, y_true
-    y_pred = torch.empty(0, num_outputs, device=DEVICE, requires_grad=True)
-    y_true = torch.empty(0, 1, device=DEVICE, dtype=torch.int8)
-
-    for fine_class_data in class_wise_logits.values():
-        t = fine_class_data
-        y_pred = torch.cat([y_pred, t.data], dim=0)
-        y_true = torch.cat([y_true, t.labels], dim=0)
-
-    y_true.squeeze_(1)
-
-    return y_pred, y_true
 
 
 class VitDecomposedTrainer(BaseTrainer):
@@ -39,6 +23,18 @@ class VitDecomposedTrainer(BaseTrainer):
 
         self.criterion = nn.CrossEntropyLoss()
 
+    def get_aggregate_loss(self, class_wise_logits: dict[str, TransformerData]):
+        all_losses = []
+
+        for op in class_wise_logits.values():
+            logits = op.data
+            labels = op.labels
+
+            if 0 not in logits.shape:
+                all_losses.append(self.criterion(logits, labels))
+
+        return torch.sum(torch.stack(all_losses))
+
     def batch_2_model_input(self, batch):
         imgs, fine_labels, _ = batch
         return {
@@ -50,16 +46,15 @@ class VitDecomposedTrainer(BaseTrainer):
         # fine is last
         num_outputs = self.model.num_leaves
         class_wise_logits = self.model(model_inputs)
-        y_pred, y_true = rearrange_outputs(class_wise_logits, num_outputs)
-        loss = self.criterion(y_pred, y_true)
+        loss = self.get_aggregate_loss(class_wise_logits)
 
         metrics = {}
 
         with torch.no_grad():
             for metric_key, metric_fxn in self.metrics_list:
                 metrics[metric_key] = metric_fxn(
-                    y_pred.clone().detach().cpu(),
-                    y_true.clone().detach().cpu(),
+                    y_pred.detach(),
+                    y_true.detach(),
                 )
 
         return [loss], metrics
