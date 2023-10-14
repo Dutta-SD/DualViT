dfbdf42193edd74292a859698ab5f743d3652721
Sandip Dutta <duttasandip11100@gmail.com>
2023-10-14 06:55:31 +0530

    newick

--- r.py
+++ r.py
@@ -1,10 +1,5 @@
-import torch
-from tp_model import TP_MODEL_MODIFIED_CIFAR10
-from vish.lightning.modulev2 import BroadFineModelLM
-from vish.utils import test_dl
+import newick
 
-
-if __name__ == "__main__":
-    CKPT_PATH = "logs/cifar100/modified_dual_tpvit_fulldataset/lightning_logs/version_1/checkpoints/tpdualvitcifar100-epoch=71-val_acc_fine=0.864.ckpt"
-    ck = torch.load(CKPT_PATH)['state_dict']
-    print(*ck.keys(), sep = "\n")
+with open("data/imagenet_meta/ref.nwk", "r") as f:
+    root = newick.load(f)[0]
+    print(root.ascii_art())
